999e7cb91522e2d9881e96ee45dc3fba12790d21
Brian Flad <bflad417@gmail.com>
2012-09-14 10:22:45 -0400

    Initial ShibbolethSP LWRP

---
+++ providers/default.rb
@@ -0,0 +1,31 @@
+#
+# Cookbook Name:: shibboleth-sp
+# Providers:: shibboleth-sp
+#
+# Copyright 2012
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+action :create do
+  template "#{node['shibboleth-sp']['dir']}/shibboleth2.xml" do
+    source params['template']
+    owner "root" unless platform? 'windows'
+    group "root" unless platform? 'windows'
+    mode "0644"
+    if params['cookbook']
+      cookbook params['cookbook']
+    end
+  end
+  notifies :restart, resources(:service => "shibd"), :delayed
+end
